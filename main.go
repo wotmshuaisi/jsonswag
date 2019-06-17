3d44a5edd54c8b5456918ec8f5eeafbb3e571f49
Charlie <charlanders1@gmail.com>
2019-06-17 16:32:02 +0800

    ignore empty request or response

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 	s := newSwagFile("./plain.txt")
 	s.GetTitle()
 	s.GetPath()
+	s.GetPath()
 	var d, _ = json.Marshal(s.Result)
 	// var d, _ = json.MarshalIndent(s.Result, "", "  ")
 	fmt.Println(string(d))
