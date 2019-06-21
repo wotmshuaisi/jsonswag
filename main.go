542f824d7d0580c8d18121a7d6e114233b402c50
wotmshuaisi <16408269+wotmshuaisi@users.noreply.github.com>
2019-06-21 22:33:51 +0800

    temporary commit for recognize url params and path params

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 	s.GetPath()
 	s.GetPath()
 	s.GetPath()
+	s.GetPath()
 	var d, _ = json.Marshal(s.Result)
 	// var d, _ = json.MarshalIndent(s.Result, "", "  ")
 	fmt.Println(string(d))
